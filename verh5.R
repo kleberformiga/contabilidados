069d075323d935b9a57597daa37674eaf70102b6
kleberformiga <mirandakf@ufersa.edu.br>
2019-06-14 21:51:37 -0300

    teste

--- verh5.R
+++ verh5.R
@@ -1,7 +1,11 @@
 verH5 <- function(search){
 
+  journal <- sub("&", "%26", search)
+  journal <- sub("@", "%40", journal)
+  journal <- str_replace_all(journal, " ", "+")
+
   #Specifying the url for desired website to be scrapped
-  url <- paste0("https://scholar.google.com/citations?hl=pt-BR&view_op=search_venues&vq=", search, "&btnG=")
+  url <- paste0("https://scholar.google.com/citations?hl=pt-BR&view_op=search_venues&vq=", journal, "&btnG=")
 
   #Reading the html content from Scholar
   webpage <- read_html(url)
