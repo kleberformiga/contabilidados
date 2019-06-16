17c6237ed48eba432010c692acb88d530ac2b4f2
kleberformiga <mirandakf@ufersa.edu.br>
2019-06-15 22:25:41 -0300

    teste

--- verh5.R
+++ verh5.R
@@ -1,8 +1,11 @@
 verH5 <- function(search){
 
-  journal <- sub("&", "%26", search)
-  journal <- sub("@", "%40", journal)
-  journal <- dQuote(str_replace_all(journal, " ", "+"))
+  journal <- sub("&", "%26", search) # Ajusta o & comercial para o codigo real
+  journal <- sub("@", "%40", journal) # Ajusta o @ arroba para o codigo real
+  journal <- sub(" *\\(.*", "", journal) # não inclui a partir de qualquer parentese
+  journal <- sub("\\.", "", journal) # retirar pontos
+  journal <- sub(":", "", journal) # retirar dois pontos
+  journal <- dQuote(str_replace_all(journal, " ", "+")) # coloca sempre entre aspas
 
   #Specifying the url for desired website to be scrapped
   url <- paste0("https://scholar.google.com/citations?hl=pt-BR&view_op=search_venues&vq=", journal, "&btnG=")
