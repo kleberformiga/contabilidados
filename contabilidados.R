b2da47a022e8b6852de8d172be0b84a73fba82d7
Kléber Formiga Miranda <mirandakf@ufersa.edu.br>
2020-02-27 23:08:30 -0300

    Update contabilidados.R

--- contabilidados.R
+++ contabilidados.R
@@ -265,8 +265,23 @@ cntdd.uneMatrix <- function(Arquivo, SeqVarPlan, index, clsPer, clsVlr){
   
 }
 
+cntdd.theme <- theme(legend.position = "bottom", legend.title = element_blank(),
+                     axis.title.x = element_blank(), axis.title.y = element_blank(),
+                     axis.text.y = element_text(color = "snow4"),
+                     axis.text.x = element_text(color = "snow4"),
+                     plot.title = element_text(color = 'lightblue3', size = 15),
+                     plot.subtitle = element_text(color = "snow4", size = 10),
+                     plot.caption = element_text(color = "snow3", size = 8),
+                     panel.background = element_blank(),
+                     panel.grid = element_line(color = "snow2"))
 
 mes.nome <- c("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho",
               "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
 
 mes.abb  <- substr(mes.nome, 1, 3)
+
+meses <- data.frame(mes.num = 1:12, mes.nome = mes.nome, mes.abb = mes.abb, month.name = month.name, month.abb = month.abb)
+  
+rm(mes.nome, mes.abb)
+
+
