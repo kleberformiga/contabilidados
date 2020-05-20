c59ff961a2e853ac4d9ff7fab943f43701afe319
Kléber Formiga Miranda <mirandakf@ufersa.edu.br>
2020-05-20 01:05:03 -0300

    Ajuste dataframe
    Transformados elementos da lista bds para data.frame, pois o merge estava dando erro na data

--- contabilidados.R
+++ contabilidados.R
@@ -257,6 +257,7 @@ cntdd.uneMatrix <- function(Arquivo, SeqVarPlan, index, clsPer, clsVlr){
   
   cntdd.variosMatrix(Arquivo = Arquivo, SeqVarPlan = SeqVarPlan, index = index, clsPer = clsPer, clsVlr = clsVlr)
   
+  bds <- lapply(bds, data.frame)
   bdPainel <<- Reduce(merge, bds)
   
   cntdd.limpaMatrix()
